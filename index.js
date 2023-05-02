af9e424b264e687f3499300ec6806a70bfd54a95
Mani <manikantavaddi4070@gmail.com>
2023-05-02 11:25:33 +0000

    superOverLeagueApp

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="img-container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="image"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
